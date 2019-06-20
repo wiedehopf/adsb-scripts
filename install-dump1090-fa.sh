2588ac7365036282ae6fc81797010cc3dd8be59c
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-20 18:06:48 +0000

    fix gain helper

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -46,7 +46,7 @@ gain=$(echo $1 | tr -cd '[:digit:].-')
 if [[ $gain == "" ]]; then echo "Error, invalid gain!"; exit 1; fi
 if [ -f /boot/piaware-config.txt ]
 then
-	sudo piaware-config rtlsdr-gain 20
+	sudo piaware-config rtlsdr-gain $gain
 fi
 if ! grep gain /etc/default/dump1090-fa &>/dev/null; then sed -i -e 's/RECEIVER_OPTIONS="/RECEIVER_OPTIONS="--gain 49.6 /' /etc/default/dump1090-fa; fi
 sudo sed -i -E -e "s/--gain .?[0-9]*.?[0-9]* /--gain $gain /" /etc/default/dump1090-fa
