fa1367ff1b68a800f957ec354d42522998cc4484
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-23 13:42:01 +0000

    move power message

--- rtl_test.sh
+++ rtl_test.sh
@@ -27,14 +27,6 @@ then
 	exit 1
 fi
 
-if [[ "throttled=0x0" != $(vcgencmd get_throttled) ]]
-then
-	dmesg --ctime | grep voltage
-	echo "Your power supply is not adequate, consider the Official Raspberry Pi power supply."
-	echo "Any constant voltage supply with 5.1 to 5.2 Volts and 2.5A capability is also a good choice."
-	echo "-------"
-	echo "Inadequate power supplies can lead to many different problems!"
-fi
 
 echo "-----"
 echo "Starting 30 second rtl_test, standby!"
@@ -56,4 +48,20 @@ then
 	do
 		systemctl restart $i 2>/dev/null
 	done
+fi
+
+
+if [[ "throttled=0x0" != $(vcgencmd get_throttled) ]]
+then
+	dmesg --ctime | grep voltage
+	echo "Your power supply is not adequate, consider the Official Raspberry Pi power supply."
+	echo "Any constant voltage supply with 5.1 to 5.2 Volts and 2.5A capability is also a good choice."
+	echo "-------"
+	echo "Inadequate power supplies can lead to many different problems!"
+else
+	echo "-------"
+	echo "-------"
+	echo "No undervoltage detected, looking fine!"
+	echo "If the dongle is not directly plugged into the Raspberry Pi, lack of power/voltage could still be an issue."
+	echo "Even without detected undervoltage a better power supply can often improve reception!"
 fi
