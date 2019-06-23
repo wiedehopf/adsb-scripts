052bc99d77072416ce5d12a63dbf94727660c840
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-23 15:44:54 +0000

    change wording, suppress some errors

--- rtl_test.sh
+++ rtl_test.sh
@@ -8,7 +8,7 @@ fi
 
 stop="piaware dump1090-fa dump1090-mutability dump1090"
 
-systemctl stop fr24feed
+systemctl stop fr24feed &>/dev/null
 
 
 if pgrep -a dump1090
@@ -29,18 +29,19 @@ fi
 
 
 echo "-----"
+echo "Lost samples in the first 2 seconds after starting the test are common and not a problem!"
 echo "Starting 30 second rtl_test, standby!"
 echo "-----"
 
 timeout 30 rtl_test -s 2400000
 
-echo "-----"
-echo "Test finished, there should be no or only few lost samples!"
-echo "Many lost samples or other errors probably mean the receiver isn't working correctly."
+echo "-------"
+echo "Test finished!"
+echo "More than 2 lost samples per million or other errors probably mean the receiver isn't working correctly."
 echo "Try another power supply before condemning the receiver though!"
-echo "-----"
+echo "-------"
 
-systemctl restart fr24feed
+systemctl restart fr24feed &>/dev/null
 
 if [[ $restart == yes ]]
 then
@@ -53,15 +54,18 @@ fi
 
 if [[ "throttled=0x0" != $(vcgencmd get_throttled) ]]
 then
+	echo "-------"
 	dmesg --ctime | grep voltage
+	echo "-------"
 	echo "Your power supply is not adequate, consider the Official Raspberry Pi power supply."
 	echo "Any constant voltage supply with 5.1 to 5.2 Volts and 2.5A capability is also a good choice."
-	echo "-------"
 	echo "Inadequate power supplies can lead to many different problems!"
-else
 	echo "-------"
+else
 	echo "-------"
 	echo "No undervoltage detected, looking fine!"
 	echo "If the dongle is not directly plugged into the Raspberry Pi, lack of power/voltage could still be an issue."
 	echo "Even without detected undervoltage a better power supply can often improve reception!"
+	echo "For optimum performance i would recommend the Official Raspberry Pi power supply."
+	echo "-------"
 fi
