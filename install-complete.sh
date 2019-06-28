b6e592d0bc3207c5c8cfb053746ca880aafec55a
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-28 08:40:39 +0000

    Bundle installation

---
+++ install-complete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/install-dump1090-fa.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/dump1090-fa-autogain.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/install.sh)"
---
+++ uninstall-complete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/uninstall-dump1090-fa.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/uninstall-dump1090-fa-autogain.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/uninstall.sh)"
