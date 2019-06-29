5300869ea511dbe330fd9a06feffe48451dd2016
Matthias Wirth <matthias.wirth@gmail.com>
2019-06-29 10:53:48 +0000

    purge dump1090-fa on uninstall

--- uninstall-dump1090-fa.sh
+++ uninstall-dump1090-fa.sh
@@ -5,6 +5,7 @@ ipath=/usr/local/share/adsb-wiki
 systemctl stop fr24feed
 
 apt-get remove -y dump1090-fa
+apt-get purge -y dump1090-fa
 rm -f /etc/lighttpd/conf-enabled/89-dump1090-fa.conf
 rm -f /etc/lighttpd/conf-enabled/88-dump1090-fa-statcache.conf
 
