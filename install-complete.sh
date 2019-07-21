c74102187032afd04b120a05d81b753016c19869
Matthias Wirth <matthias.wirth@gmail.com>
2019-07-21 10:51:49 +0000

    remove @ and http in output to be better suitable for fr24 forum

--- install-complete.sh
+++ install-complete.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+clear
+echo "--------------"
+echo "Bundle install for dump1090-fa by wiedehopf
+echo "--------------"
 
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/install-dump1090-fa.sh)"
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/dump1090-fa-autogain.sh)"
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/install.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/install-dump1090-fa.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/dump1090-fa-autogain.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/install.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
