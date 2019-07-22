69776d64e6a9d17901e2a3a03afe531f20a43af4
Matthias Wirth <matthias.wirth@gmail.com>
2019-07-22 07:54:24 +0000

    fix an error

--- install-complete.sh
+++ install-complete.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 clear
+set -e
 echo "--------------"
-echo "Bundle install for dump1090-fa by wiedehopf
+echo "Bundle install for dump1090-fa by wiedehopf"
 echo "--------------"
+
 
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/install-dump1090-fa.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/dump1090-fa-autogain.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
