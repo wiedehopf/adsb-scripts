32f890132d4b82832d7e5f2f90f7aa38b07e7024
Matthias Wirth <matthias.wirth@gmail.com>
2019-08-07 22:03:19 +0200

    more stable version of the script

--- log-pos.sh
+++ log-pos.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 view1090-fa --no-interactive \
-	| grep -F -e CPR -B7 \
-	| grep -F -e RSSI -e latitude -e longitude -e altitude: \
-	| awk '{c="\n"} NR%4 {c=""} { printf("%s%s", $0, c) } ' \
-	| sed -u -e 's/  Baro altitude: //' -e 's/  CPR latitude:  //' \
-	-e 's/  CPR longitude: //' -e 's/RSSI: //' -e 's/ dBFS/,/' \
-	-e 's/ ([0-9]*)/,/g' -e 's/ ft/,/' -e 's/,$//' \
-	| sed '/([0-9]*)/d'
+	| grep -F -e 'CPR longitude' -B10 \
+	| grep -F -e '--' -e RSSI -e latitude -e longitude -e altitude: \
+	| awk '!/--$/ { printf("%s\t", $0); next } 1' \
+	| sed \
+		-e 's/RSSI: //' \
+		-e 's/ dBFS	  Baro altitude: /,/' \
+		-e 's/ ft	  CPR latitude:  /,/' \
+		-e 's/ ([0-9]*)	  CPR longitude: /,/' \
+		-e 's/ ([0-9]*)	--//' \
+	| sed '/ /d'
