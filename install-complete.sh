f70da292fe3aa61be28ab4ea90d23fb9222e1c7c
Matthias Wirth <matthias.wirth@gmail.com>
2019-08-10 16:35:15 +0200

    fix URLs

--- install-complete.sh
+++ install-complete.sh
@@ -5,6 +5,6 @@ echo "Bundle install for dump1090-fa by wiedehopf"
 echo "--------------"
 
 
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/install-dump1090-fa.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/dump1090-fa-autogain.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/install-dump1090-fa.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/dump1090-fa-autogain.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/install.sh)" 2>&1 | sed -e 's?://?  ?' -e 's?ttp??' -e 's/@/_at_/'
