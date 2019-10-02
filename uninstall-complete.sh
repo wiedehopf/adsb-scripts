5af70e699e54a0514645556636f685d4e9713aea
Matthias Wirth <matthias.wirth@gmail.com>
2019-10-02 16:04:41 +0200

    add tar1090 interface to bundle

--- uninstall-complete.sh
+++ uninstall-complete.sh
@@ -3,3 +3,4 @@
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/uninstall-dump1090-fa.sh)"
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/uninstall-dump1090-fa-autogain.sh)"
 bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/uninstall.sh)"
+bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/tar1090/master/uninstall.sh)"
