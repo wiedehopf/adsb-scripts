c224e0afc6176747c5fd737c9e0d97da741a804d
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-19 22:25:10 +0100

    fix some autogain errors

--- autogain-install.sh
+++ autogain-install.sh
@@ -5,8 +5,8 @@ rm -f /usr/local/bin/dump1090-fa-autogain
 rm -f /etc/default/dump1090-fa-autogain
 rm -f /etc/cron.d/dump1090-fa-autogain
 
-systemctl disable dump1090-fa-autogain.timer
-systemctl stop dump1090-fa-autogain.timer
+systemctl disable dump1090-fa-autogain.timer &>/dev/null
+systemctl stop dump1090-fa-autogain.timer &>/dev/null
 
 rm -f /lib/systemd/system/dump1090-fa-autogain.service
 rm -f /lib/systemd/system/dump1090-fa-autogain.timer
@@ -18,6 +18,7 @@ cat >/usr/local/bin/autogain1090 <<"EOF"
 #!/bin/bash
 low=1.0
 high=5.0
+source /etc/default/autogain1090
 ga=(0.0 0.9 1.4 2.7 3.7 7.7 8.7 12.5 14.4 15.7 16.6 19.7 20.7 22.9 25.4 28.0 29.7 32.8 33.8 36.4 37.2 38.6 40.2 42.1 43.4 43.9 44.5 48.0 49.6 -10)
 tmp=/var/tmp/autogain1090
 mkdir -p $tmp
@@ -30,9 +31,8 @@ elif [[ -f /run/readsb/stats.json ]]; then
 fi
 
 stats=/run/$APP/stats.json
-source /etc/default/autogain1090
 
-if ! [[ -f $stats ]]; then echo "$stats not found, is the decoder running?"; exit 0; fi
+if ! [[ -f $stats ]]; then echo "stats.json not found, is the decoder running?"; exit 0; fi
 
 oldstrong=$(cat $tmp/strong 2>/dev/null)
 oldtotal=$(cat $tmp/total 2>/dev/null)
@@ -54,7 +54,11 @@ fi
 strong=$((strong - oldstrong))
 total=$((total - oldtotal))
 
-percent=$(awk "BEGIN {printf \"%.3f\", $strong * 100 / $total}")
+if [[ $total == 0 ]]; then
+    percent=0
+else
+    percent=$(awk "BEGIN {printf \"%.3f\", $strong * 100 / $total}")
+fi
 
 strong=$percent
 
@@ -63,25 +67,45 @@ oldgain=$(grep -P -e 'gain \K[0-9-.]*' -o /etc/default/$APP)
 
 gain_index=28
 for i in "${!ga[@]}"; do
-	if [[ "${ga[$i]}" = "${oldgain}" ]]; then gain_index="${i}"; fi
+    if ! awk "BEGIN{ exit  (${oldgain} <= ${ga[$i]}) }"; then
+        gain_index="${i}"
+        break
+    fi
 done
+
+if ! awk "BEGIN{ exit  (${oldgain} > 49.6) }"; then
+    gain_index=28
+fi
+
+if [[ "$oldgain" == "-10" ]]; then
+    gain_index=29
+fi
+
 
 if ! awk "BEGIN{ exit ($strong > $low) }" && ! awk "BEGIN{ exit ($strong < $high) }"; then
 	echo "No gain change needed, percentage of messages >-3dB is in nominal range. (${strong}%)"
 	exit 0
 fi
 
-if ! awk "BEGIN{ exit ($strong < $low) }" && [[ $gain_index == 28 ]]
-then echo "Gain already at maximum! (${strong}% messages >-3dB)"; exit 0; fi
+if ! awk "BEGIN{ exit ($strong < $low) }" && [[ $gain_index == 29 ]]; then
+    echo "Gain already at maximum! (${strong}% messages >-3dB)"
+    exit 0
+fi
 
-if ! awk "BEGIN{ exit ($strong < $low) }"
-then gain_index=$(($gain_index+1)); action=Increasing; fi
+if ! awk "BEGIN{ exit ($strong < $low) }"; then
+    gain_index=$(($gain_index+1))
+    action=Increasing
+fi
 
-if ! awk "BEGIN{ exit ($strong > $high) }" && [[ $gain_index == 0 ]]
-then echo "Gain already at minimum! (${strong}% messages >-3dB)"; exit 0; fi
+if ! awk "BEGIN{ exit ($strong > $high) }" && [[ $gain_index == 0 ]]; then
+    echo "Gain already at minimum! (${strong}% messages >-3dB)"
+    exit 0
+fi
 
-if ! awk "BEGIN{ exit ($strong > $high) }"
-then gain_index=$(($gain_index-1)); action=Decreasing; fi
+if ! awk "BEGIN{ exit ($strong > $high) }"; then
+    gain_index=$(($gain_index-1))
+    action=Decreasing
+fi
 
 gain="${ga[$gain_index]}"
 
@@ -93,7 +117,7 @@ then
 fi
 
 if ! grep gain /etc/default/$APP &>/dev/null; then sed -i -e 's/RECEIVER_OPTIONS="/RECEIVER_OPTIONS="--gain 49.6 /' /etc/default/$APP;fi
-sed -i -E -e "s/--gain .?[0-9]*.?[0-9]* /--gain $gain /" /etc/default/$APP
+sed -i -E -e "s/--gain -?[0-9]*\.?[0-9]*/--gain $gain/" /etc/default/$APP
 
 systemctl restart $APP
 
@@ -121,7 +145,7 @@ rm -f /etc/cron.d/autogain1090
 
 cat >/lib/systemd/system/autogain1090.service <<"EOF"
 [Unit]
-Description=autogain for dump1090-fa
+Description=autogain for readsb or dump1090-fa
 
 [Service]
 ExecStart=/usr/local/bin/autogain1090
@@ -129,7 +153,7 @@ EOF
 
 cat >/lib/systemd/system/autogain1090.timer <<"EOF"
 [Unit]
-Description=Nightly automic gain adjustment for dump1090-fa
+Description=Nightly automic gain adjustment for readsb or dump1090-fa
 
 [Timer]
 OnCalendar=*-*-* 02:30:00
--- autogain-remove.sh
+++ autogain-remove.sh
@@ -4,8 +4,8 @@ rm -f /usr/local/bin/autogain1090
 rm -f /etc/default/autogain1090
 rm -f /etc/cron.d/autogain1090
 
-systemctl disable autogain1090.timer
-systemctl stop autogain1090.timer
+systemctl disable autogain1090.timer &>/dev/null
+systemctl stop autogain1090.timer &>/dev/null
 
 rm -f /lib/systemd/system/autogain1090.service
 rm -f /lib/systemd/system/autogain1090.timer
@@ -16,8 +16,8 @@ rm -f /usr/local/bin/dump1090-fa-autogain
 rm -f /etc/default/dump1090-fa-autogain
 rm -f /etc/cron.d/dump1090-fa-autogain
 
-systemctl disable dump1090-fa-autogain.timer
-systemctl stop dump1090-fa-autogain.timer
+systemctl disable dump1090-fa-autogain.timer &>/dev/null
+systemctl stop dump1090-fa-autogain.timer &>/dev/null
 
 rm -f /lib/systemd/system/dump1090-fa-autogain.service
 rm -f /lib/systemd/system/dump1090-fa-autogain.timer
