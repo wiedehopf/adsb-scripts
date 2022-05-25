1793f41f3c60e0d989ffcd4421a3b13d9dda7f2f
Matthias Wirth <matthias.wirth@gmail.com>
2022-05-25 15:44:19 +0200

    fr24 bs

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -87,7 +87,8 @@ then
     if ! grep -e 'host=' /etc/fr24feed.ini &>/dev/null; then echo 'host=' >> /etc/fr24feed.ini; fi
     if ! grep -e 'receiver=' /etc/fr24feed.ini &>/dev/null; then echo 'receiver=' >> /etc/fr24feed.ini; fi
 
-    sed -i -e 's/receiver=.*/receiver="beast-tcp"/' -e 's/host=.*/host="127.0.0.1:30005"/' -e 's/bs=.*/bs="no"/' -e 's/raw=.*/raw="no"/' /etc/fr24feed.ini
+    sed -i -e 's/receiver=.*/receiver="beast-tcp"/' -e 's/host=.*/host="127.0.0.1:30005"/' \
+        -e 's/mlat=.*/mlat="no"/' -e 's/bs=.*/bs="no"/' -e 's/raw=.*/raw="no"/' /etc/fr24feed.ini
 
     systemctl restart fr24feed &>/dev/null || true
 fi
