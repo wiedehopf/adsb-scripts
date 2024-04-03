1554b8d132d66951dadb7c59c8f73dc3dbdaf306
Matthias Wirth <matthias.wirth@gmail.com>
2024-04-03 18:59:30 +0200

    up fr24feed version
    fixes: https://github.com/wiedehopf/adsb-scripts/issues/38

--- fr24-nopackage.sh
+++ fr24-nopackage.sh
@@ -20,7 +20,7 @@ rm /tmp/fr24 -rf
 mkdir -p /tmp/fr24
 cd /tmp
 
-wget -O fr24.deb https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_1.0.34-0_armhf.deb
+wget -O fr24.deb https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_1.0.46-2_armhf.deb
 
 dpkg -x fr24.deb fr24
 cp -f fr24/usr/bin/fr24feed* /usr/bin
